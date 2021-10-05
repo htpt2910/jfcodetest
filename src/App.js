a9192d8adf793e7f8c9d5424f926e2e403d1cd1d
htpt2910 <hmc2910@gmail.com>
2021-10-05 21:50:38 +0700

    implementing handleChange, still stuck at the checked event

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-vars */
 import Item from "./components/item";
 import {
   LineChart,
@@ -11,10 +13,14 @@ import {
 import React from "react";
 import { useEffect, useState } from "react";
 import "./App.css";
-function App() {
-  function fetchData(prefCode, cityCode) {
-    // GET request using fetch inside useEffect React hook
+import useCheckboxes from "./components/checkboxes";
+// import { adaptEventHandlers } from "recharts/types/util/types";
 
+
+function App() {
+
+
+  function fetchData(prefCode, cityCode) {
     fetch(
       `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`,
       {
@@ -30,77 +36,40 @@ function App() {
           status: response.status,
         }))
         .then((res) => {
-          const arr = []
           var val = res.data.result.data[0];
-          var len = val.data.length;
-          const kekka = val.data.map((element) => element.value);
-          console.log(kekka);
-          console.log(res.data, res.status);
-          // setValue(res.data.result.data[0].label);
+          const result = val.data.map((element) => element.value);
+          console.log(result);
         })
     );
   }
 
   // function execute(state){ //TODO
-  //   for ... in state():
+  //   for(var i=0; i < state.length; i++){
   //     if (state[i] == checked){
   //       fetchData(i);
   //     }
+  //   }
+      
   // }
 
-  // const [value, setValue] = useState(null);
-  // const [state, setState] = useState(...); // TODO 
-  // useEffect(() => execute(curSate), []);
+  const [isCheck, setIsCheck] = useState(false);
+  const [prefCodes, setPrefCodes] = useState([]);
 
-  const cityNames = [
-    "北海道",
-    "青森県",
-    "岩手県",
-    "宮城県",
-    "秋田県",
-    "山形県",
-    "福島県",
-    "茨城県",
-    "栃木県",
-    "群馬県",
-    "埼玉県",
-    "千葉県",
-    "東京都",
-    "神奈川県",
-    "新潟県",
-    "富山県",
-    "石川県",
-    "福井県",
-    "山梨県",
-    "長野県",
-    "岐阜県",
-    "静岡県",
-    "愛知県",
-    "三重県",
-    "滋賀県",
-    "京都府",
-    "大阪府",
-    "兵庫県",
-    "奈良県",
-    "和歌山県",
-    "鳥取県",
-    "島根県",
-    "岡山県",
-    "広島県",
-    "山口県",
-    "徳島県",
-    "香川県",
-    "愛媛県",
-    "高知県",
-    "福岡県",
-    "佐賀県",
-    "長崎県",
-    "熊本県",
-    "大分県",
-    "宮崎県",
-    "鹿児島県",
-    "沖縄県",
-  ];
+  const isCheckArray = [];
+  function handleChange(event, index){
+    setIsCheck(event.target.checked);  //TODO
+    console.log(isCheck);
+    // console.log(index);
+    // index = index + 1;
+    // console.log("is check in handler : " + isCheck)
+    // if(isCheck)
+    //   setPrefCodes(preCodes => [...preCodes, index]);
+
+    // console.log(prefCodes);
+  }
+
+  // const [state, setState] = useState([]); // TODO 
+  // useEffect(() => execute(prefCodes), []);
 
   const data = [
     {
@@ -114,32 +83,11 @@ function App() {
       uv: 3000,
       pv: 1398,
       amt: 2210,
-    },
-    {
-      name: "2000",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "2010",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "2020",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "年度",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
+    }
   ];
+
+  const checkboxes = useCheckboxes();
+
   return (
     <div className="App">
       <header>
@@ -148,27 +96,31 @@ function App() {
       <hr />
       <p className="caption">都道府県</p>
       <div className="list-city-name">
-        {cityNames.map((city) => (
-          <Item cityName={city} />
-        ))}
+        <div>
+          {checkboxes.map((item, index) => 
+            <Item key={index} id={index} check={item.checked} cityName={item.name} onChange={(event) => {
+              handleChange(event,index);
+            }}/>
+          )}
+        </div>
       </div>
-      <div className="chart">
-        <LineChart
-          width={1000}
-          height={300}
-          data={data}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis className="year" dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </div>
-      <div>{/* Total react packages: {value} */}</div>
+        {/* <div className="chart">
+          <LineChart
+            width={1000}
+            height={300}
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis className="year" dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </div> */}
+        
     </div>
   );
 }
--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
+// import { Checkbox } from '@material-ui/core';
+// import { FormControlLabel } from '@material-ui/core';
+import React from 'react';
+import label from 'react';
+import input from 'react';
+import { propTypes } from 'react-bootstrap/esm/Image';
 
-function Item(props) {
-    return (
-        <div className="item">
-            <input className="check-box" type="checkbox" />
-            <label className="city-name" >{props.cityName}</label>
-        </div>
-    )
+function Item( props ) {
+
+  return (
+    <>
+        <label>
+          <input
+            type="checkbox"
+            checked={props.checked}
+            onChange={props.onChange}
+          />
+          {props.cityName}
+        </label>
+      
+      
+    </>
+  );
 }
 
 export default Item;
